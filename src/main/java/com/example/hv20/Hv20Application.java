ad11828c6deb52c14de79f6a06147690840e5955
weistapki <weistapki@gmail.com>
2023-07-04 19:45:09 +0200

    Initial commit

---
+++ src/main/java/com/example/hv20/Hv20Application.java
@@ -0,0 +1,13 @@
+package com.example.hv20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hv20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hv20Application.class, args);
+	}
+
+}
